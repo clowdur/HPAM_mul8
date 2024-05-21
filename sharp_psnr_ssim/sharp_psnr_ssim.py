d881793b66b24d94ba0f501f763ca1766f1a2466
clowdur <greenohlbur@gmail.com>
2024-05-21 15:25:17 -0700

    Python Sharpening Script with PSNR, SSIM metrics; adapted from matlab code from HPAM team

---
+++ sharp_psnr_ssim/sharp_psnr_ssim.py
@@ -0,0 +1,151 @@
+# Oliver Huang, Jared Yoder
+# Spring 2024
+# Image Sharpening Algorithm for Approximate Multiplier benchmark
+# Adapted from matlab code provided by the HPAM researchers
+
+import cv2
+# from google.colab.patches import cv2_imshow
+import numpy as np
+
+import math
+import cv2
+import os
+import argparse
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+from skimage.util import random_noise
+from skimage.metrics import structural_similarity as SSIM
+import skimage
+
+
+lut = []
+lutname = 'LUT4.txt'
+# lutname = 'LUT2.txt'
+# lutname = 'LUTW.txt'
+with open(lutname) as f:
+    for line in f:
+        words = line.split()
+        lut.append([int(x) for x in words])
+
+# Read an image
+filename = 'jared.jpeg'
+# filename = 'green.png'
+# filename = 'biden.jpg'
+img = cv2.imread(filename)
+processed = 0
+print( "\
+┌────────────────────────────────────────────────────┐\n\
+│                                                    │\n\
+│   ╔╗──────────╔═╦╗────────────╔╗                   │\n\
+│   ╠╬══╦═╗╔═╦═╗║═╣╚╦═╗╔╦╦═╦═╦═╦╬╬═╦╦═╗              │\n\
+│   ║║║║║╬╚╣╬║╩╣╠═║║║╬╚╣╔╣╬║╩╣║║║║║║║╬║  █ █ █   ▄▀  │\n\
+│   ╚╩╩╩╩══╬╗╠═╝╚═╩╩╩══╩╝║╔╩═╩╩═╩╩╩═╬╗║  ▀▄▀▄▀ ▄▀    │\n\
+│   ───────╚═╝───────────╚╝─────────╚═╝              │\n\
+│   ********************************************     │\n\
+│       ██╗░░██╗██████╗░░█████╗░███╗░░░███╗          │\n\
+│       ██║░░██║██╔══██╗██╔══██╗████╗░████║          │\n\
+│       ███████║██████╔╝███████║██╔████╔██║          │\n\
+│       ██╔══██║██╔═══╝░██╔══██║██║╚██╔╝██║          │\n\
+│       ██║░░██║██║░░░░░██║░░██║██║░╚═╝░██║          │\n\
+│       ╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝          │\n\
+│   ********************************************     │\n\
+│                               by Oliver Huang      │\n\
+│                                &  Jared Yoder      │\n\
+└────────────────────────────────────────────────────┘")
+print("Input image: %s" % filename)
+print("Approximate Multiplier Used: %s" % lutname)
+
+# noisy_img = skimage.img_as_ubyte(random_noise(img, mode='gaussian', seed=None, clip=True))
+
+# Kernel
+Kernel = np.array([[1,  4,  7,  4, 1],
+                   [4, 16, 26, 16, 4],
+                   [7, 26, 41, 26, 7],
+                   [4, 16, 26, 16, 4],
+                   [1,  4,  7,  4, 1]])
+
+# Pad the image with zeros # Oliver: this ends up causing white edges on the image. A flaw of their algorithm.
+I = np.pad(img, ((2, 2), (2, 2), (0, 0)), mode='constant', constant_values=0)
+# Initialize output arrays
+output_accurate = np.zeros(I.shape, dtype=float)
+output_approx = np.zeros(I.shape, dtype=float)
+iy, ix, icolor = img.shape
+y, x, color = I.shape
+
+print()
+
+#  Convolution
+for color_channel in range(color):
+    for i in range(2, y - 2):
+        for j in range(2, x - 2):
+
+            # get a section of image from a current channel to multiply with
+            # the kernel
+            Img_section = I[i-2:i+3, j-2:j+3, color_channel]
+
+            # ===================== ACCURATE COMPUTATION ===================
+            # Element-by-element multiplication
+            temp_multiply_acc = Img_section * Kernel
+
+            # % Accumulation
+            # temp_accumulate_acc = sum(temp_multiply_acc(:));
+            temp_accumulate_acc = np.sum(temp_multiply_acc)
+
+            # ==================== APPROXIMATE COMPUTATION =================
+            # Element-by-element multiplication & accumulation
+            accumulator = 0
+            ky, lx = Img_section.shape
+            for k in range(ky):
+                for l in range(lx):
+                    accumulator = accumulator + lut[Img_section[k][l]][Kernel[k][l]]
+            output_accurate[i, j, color_channel] = 2 * I[i, j, color_channel] - temp_accumulate_acc / 273
+            output_approx[i, j, color_channel] = 2 * I[i, j, color_channel] - accumulator / 273
+
+        processed += ix
+        # print("Processed Pixels: %d; %d%% Done" % (processed, math.ceil(100*(float(processed)/(x*y*3)))), end="\r")
+        print("Processed Pixels: %d; %d%% Done" % (processed, int(100*(float(processed)/(ix*iy*icolor)))), end="\r")
+            
+            
+
+
+# Unpadding (removing 2 rows & 2 colomns of zeros from each side)
+output_acc = output_accurate[2:I.shape[0]-2, 2:I.shape[1]-2, :]
+output_app = output_approx[2:I.shape[0]-2, 2:I.shape[1]-2, :]
+
+# Convert to uint8, prevent clipping/peaking
+output_acc = np.maximum(output_acc, np.zeros(output_acc.shape))
+output_acc = np.minimum(output_acc, 255 * np.ones(output_acc.shape))
+output_acc = output_acc.round().astype(np.uint8)
+output_app = np.maximum(output_app, np.zeros(output_app.shape))
+output_app = np.minimum(output_app, 255 * np.ones(output_app.shape))
+output_app = output_app.round().astype(np.uint8)
+
+# Display and save images
+ogs = np.concatenate((img, output_acc), axis=1)
+res = np.concatenate((ogs, output_app), axis=1)
+# comparing original vs resized
+print('\nOriginal, Exact Sharpen, Approx Sharpen')
+cv2.namedWindow("results", cv2.WINDOW_NORMAL) 
+# cv2.resizeWindow("results", 1920, 1080)
+cv2.imshow("results", res)
+cv2.imshow("apprx", output_app)
+# Resize the Window
+
+
+
+# Calculate PSNR and SSIM
+psnr = cv2.PSNR(output_acc, output_app)
+print("PSNR:", psnr) #OpenCV prevents divide by 0 and has no 'Inf' so identical images will have PSNR of 361.20199909921956
+ssim = SSIM(output_acc, output_app, data_range=output_app.max() - output_app.min(), multichannel=True)
+#ssim = SSIM(output_acc, output_app, data_range=output_app.max() - output_app.min(), channel_axis=2) #newer versions
+print("SSIM:", ssim)
+print("\n[Ctrl+C to exit]")
+cv2.imwrite('accurate_sharp.bmp', output_acc)
+cv2.imwrite('approx_sharp.bmp', output_app)
+while cv2.getWindowProperty('results', 0) >= 0:
+    keyCode = cv2.waitKey(50)
+
+
+cv2.destroyAllWindows()
+# cv2.waitKey(1)
+quit()
--- wallace_tree/v/testbench.v
+++ wallace_tree/v/testbench.v
@@ -31,18 +31,30 @@ module testbench;
   end // initial clock
 
   mult_wrapper dut (.Y(result), .inA(a), .inB(b), .clk(clk));
-  //wallaceTreeMultiplier8Bit dut (.*);
   
   integer i, j;
+  integer fd;
   initial tot_error = 0;
   initial begin
+    
+    // open lookup table file
+    fd = $fopen("LUT.txt", "w");
+    if (fd) $display("LUT file was opened successfully");
+    else $display("LUT file was not opened successfully");
+
     $write("%c[1;32m",27); $display("\nBegin: Multiplier Test Cases\n");
     $write("%c[0m",27);
     for(i = 0; i < 2**8; i++) begin
+      // write a newline to LUT if not the first row of results
+      if (i && fd) $fwrite(fd, "\n");
       for (j = 0; j < 2**8; j++) begin
         a <= i; b <= j; @(posedge clk); @(posedge clk); @(posedge clk); //3 cycles for update (1: load input reg D, 2: compute & load output reg D, 3: see output reg Q)
         $display("inputs: a:%d, b:%d", a, b); 
         $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); 
+
+        // write results to lookup table file
+        if (fd) $fwrite(fd, "%d ", result);
+        
         if (rel_error < 0) tot_error -= rel_error;
         else tot_error += rel_error; //65536 iterations
       end  
@@ -54,35 +66,5 @@ module testbench;
     $write("%c[0m",27);
     $finish;
   end
-  
-  
-  // initial begin
-  //   $write("%c[1;32m",27); $display("\nBegin: Multiplier Test Cases\n"); @(posedge clk);
-  //   $write("%c[0m",27);
-  //   a <= 255; b <= 255; @(posedge clk); @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk); 
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 17; b <= 17; @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 23; b <= 67; @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 67; b <= 23; @(posedge clk); @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 0; b <= 19; @(posedge clk); @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 19; b <= 0; @(posedge clk); @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   a <= 0; b <= 0; @(posedge clk); @(posedge clk);
-  //   $display("inputs: a:%d, b:%d", a, b); @(posedge clk);
-  //   $display("result: %d  true: %.1f  error: %f\n", result, true_result, rel_error); @(posedge clk);
-  //   $write("%c[1;32m",27); $display("End: Multiplier Test Cases\n"); @(posedge clk);
-  //   $write("%c[0m",27);
-  //   $finish;
-  // end
 
 endmodule
